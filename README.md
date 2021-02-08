a3fa14a69d3c0e6aa44861e34f117d8a67cab563
Manisha <manisha.sharma@gmail.com>
2021-02-08 14:27:38 +0530

    added initial setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Arrays
 Practicing Arrays with Java
+
+Based on Leetcode lear card : Array chapter : (https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3221/)
+
---
+++ app/src/main/java/com/example/myapplication/arrays/DVD.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.arrays;
+
+import org.jetbrains.annotations.NotNull;
+
+public class DVD {
+    public String directorName;
+    public int yearOfPublication;
+    public String name;
+
+    public DVD( String directorName, int yearOfPublication, String name){
+        this.directorName = directorName;
+        this.yearOfPublication = yearOfPublication;
+        this.name = name;
+    }
+}
